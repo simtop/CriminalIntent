80897e27232ca8999d0936082718c06fef91f5ec
simtop <41802904+simtop@users.noreply.github.com>
2018-12-20 18:46:16 +0100

    finished temorary CrimeFragment and connected to click method of ViewHolder

--- app/src/main/java/com/example/simon/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/simon/criminalintent/CrimeActivity.java
@@ -1,8 +1,20 @@
 package com.example.simon.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID = "com.example.simon.criminalintent.crime_id";
+
+    public static Intent newIntent(Context package_context, UUID crime_id){
+        Intent intent = new Intent(package_context,CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crime_id);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/example/simon/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/simon/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.simon.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -76,9 +77,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked !!",Toast.LENGTH_SHORT ).
-                    show();
+            Intent intent =  CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            startActivity(intent);
         }
     }
 
