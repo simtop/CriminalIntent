e78860d7071d59a6fa7cfcc3f7b31fa2b17d69b5
simtop <41802904+simtop@users.noreply.github.com>
2018-12-25 16:39:34 +0100

    Dialog detects Crime Dates

--- app/src/main/java/com/example/simon/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/example/simon/criminalintent/DatePickerFragment.java
@@ -6,15 +6,48 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date,null);
+
+        mDatePicker = v.findViewById(R.id.dialog_date_picker);
+        mDatePicker.init(year,month,day,null);
+
         return new AlertDialog.Builder(getActivity()).
+                setView(v).
                 setTitle(R.string.date_picker_title).
                 setPositiveButton(android.R.string.ok, null).
                 create();
+    }
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE,date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 }
