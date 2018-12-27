27cf9335e6cefd1d1f9f1279d744f77ebe808822
simtop <41802904+simtop@users.noreply.github.com>
2018-12-27 21:34:07 +0100

    Improved toolbar subtitle part and added text and button to show when reciclerview is empty to help user to add crimes

--- app/src/main/java/com/example/simon/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/simon/criminalintent/CrimeListFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,6 +28,9 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
     private int mClickedItemPosition;
     private boolean mSubtitleVisible;
+
+    private TextView mEmptyTextView;
+    private Button mAddCrimeButton;
 
     private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
 
@@ -53,11 +57,7 @@ public class CrimeListFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.new_crime:
-                Crime crime = new Crime();
-                CrimeLab.get(getActivity()).addCrime(crime);
-                Intent intent = CrimePagerActivity
-                        .newIntent(getActivity(), crime.getId());
-                startActivity(intent);
+                createNewCrimeAndStart();
                 return true;
             case R.id.show_subtitle:
                 mSubtitleVisible = !mSubtitleVisible;
@@ -69,10 +69,20 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
+    private void createNewCrimeAndStart() {
+        Crime crime = new Crime();
+        CrimeLab.get(getActivity()).addCrime(crime);
+        Intent intent = CrimePagerActivity
+                .newIntent(getActivity(), crime.getId());
+        startActivity(intent);
+    }
+
     private void updateSubtitle() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         int crimeCount = crimeLab.getCrimes().size();
-        String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        String subtitle = getResources()
+                .getQuantityString(R.plurals.subtitle_plural, crimeCount, crimeCount);
 
         if (!mSubtitleVisible) {
             subtitle = null;
@@ -89,6 +99,15 @@ public class CrimeListFragment extends Fragment {
 
         mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mEmptyTextView = view.findViewById(R.id.empty_text);
+        mAddCrimeButton = view.findViewById(R.id.new_crime_button);
+        mAddCrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewCrimeAndStart();
+            }
+        });
 
         if (savedInstanceState != null) {
             mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
@@ -116,6 +135,14 @@ public class CrimeListFragment extends Fragment {
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
+
+        if (crimes.size() == 0) {
+            mEmptyTextView.setVisibility(View.VISIBLE);
+            mAddCrimeButton.setVisibility(View.VISIBLE);
+        } else {
+            mEmptyTextView.setVisibility(View.GONE);
+            mAddCrimeButton.setVisibility(View.GONE);
+        }
 
         if(mAdapter == null){
             mAdapter = new CrimeAdapter(crimes);
